763dd20b73cab1d26e2b8d7d36298fce4a297e8d
T31K <t31kmunwong@gmail.com>
2022-08-09 11:49:42 +0800

    feat(api): add api calls utils

---
+++ src/utils/fetchData.js
@@ -0,0 +1,15 @@
+export const fetchData = (token) => {
+  // fetch(
+  //   "https://api.spotify.com/v1/browse/featured-playlists?country=SG&locale=en_SG&limit=10",
+  //   {
+  //     headers: {
+  //       Accept: "application/json",
+  //       "Content-Type": "application/json",
+  //       Authorization: `Bearer ${token}`,
+  //     },
+  //   }
+  // ).then((res) => {
+  //   return res.json();
+  // });
+  console.log("fetchData");
+};
---
+++ src/utils/getFeaturedList.js
@@ -0,0 +1,14 @@
+export const getFeaturedList = (token) => {
+  fetch(
+    "https://api.spotify.com/v1/browse/featured-playlists?country=SG&locale=en_SG&limit=10",
+    {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  ).then((res) => {
+    return res.json();
+  });
+};
