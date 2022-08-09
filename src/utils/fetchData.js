adb210ec840c7775db0a15a742ce3251f81adb20
T31K <t31kmunwong@gmail.com>
2022-08-09 14:39:26 +0800

    refactor(api): cleanup api calls

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,15 +1,25 @@
-export const fetchData = (token) => {
-  // fetch(
-  //   "https://api.spotify.com/v1/browse/featured-playlists?country=SG&locale=en_SG&limit=10",
-  //   {
-  //     headers: {
-  //       Accept: "application/json",
-  //       "Content-Type": "application/json",
-  //       Authorization: `Bearer ${token}`,
-  //     },
-  //   }
-  // ).then((res) => {
-  //   return res.json();
-  // });
-  console.log("fetchData");
+import axios from "axios";
+
+const featuredPlaylist = async (token) => {
+  let params = {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  let url =
+    "https://api.spotify.com/v1/browse/featured-playlists?country=SG&locale=en_SG&limit=30";
+  try {
+    const res = await axios.get(url, params);
+    return res.data;
+  } catch (error) {
+    return error;
+  }
 };
+
+const fetchData = {
+  featuredPlaylist: featuredPlaylist,
+};
+
+export default fetchData;
