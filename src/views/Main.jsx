01ee67c07732325b8b6463c0ccbd89bc23b68b71
T31K <t31kmunwong@gmail.com>
2022-08-10 22:10:09 +0800

    feat(nav): add topnav

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -1,11 +1,12 @@
+import TopNav from "../components/navs/TopNav";
 import SideNav from "../components/navs/SideNav";
 import Container from "../components/app/Container";
-import { useEffect } from "react";
 
 export default function Main() {
   return (
     <>
       <div class="flex w-screen h-screen text-gray-700">
+        <TopNav />
         <SideNav />
         <Container />
       </div>
