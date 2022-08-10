957709a08993242e0d4e9c6f5c3fa6f06cc2f768
T31K <t31kmunwong@gmail.com>
2022-08-10 22:56:02 +0800

    fix(deploy): test env variables

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,11 +1,7 @@
 export default function Login() {
   const handleClick = () => {
-    const clientId = `${
-      process.env.CLIENT_ID || process.env.REACT_APP_CLIENT_ID
-    }`;
-    const clientSecret = `${
-      process.env.CLIENT_SECRET || process.env.REACT_APP_CLIENT_SECRET
-    }`;
+    const clientId = `${process.env.REACT_APP_CLIENT_ID}`;
+    const clientSecret = `${process.env.REACT_APP_CLIENT_SECRET}`;
     const redirectUrl = "http://localhost:3000/";
     const apiUrl = "https://accounts.spotify.com/authorize";
     const scope = [
