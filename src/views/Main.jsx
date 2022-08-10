e4a1d274123e0771ee5d00a72fbfb33c5a1bf5e6
T31K <t31kmunwong@gmail.com>
2022-08-10 22:42:03 +0800

    fix(deploy): remove topbar

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -1,4 +1,3 @@
-import TopNav from "../components/navs/TopNav";
 import SideNav from "../components/navs/SideNav";
 import Container from "../components/app/Container";
 
@@ -6,7 +5,6 @@ export default function Main() {
   return (
     <>
       <div class="flex w-screen h-screen text-gray-700">
-        <TopNav />
         <SideNav />
         <Container />
       </div>
